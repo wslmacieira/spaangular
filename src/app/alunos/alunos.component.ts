6cc0913de8119e9e093c2fc5da42d9f037146f27
wslmacieiradev <wslmacieira@outlook.com>
2021-02-03 11:22:11 -0300

    ajuste home component

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'spa-alunos',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunosComponent implements OnInit {
 
-  valorInicial: number = 10;
+  @Input() valorInicial: number = 10;
 
   constructor() { }
 
